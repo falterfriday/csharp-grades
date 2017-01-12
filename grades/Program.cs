db7371e0abab6e0664c87505af50b68cfa8f5f6d
Patrick Todd <patrick.a.todd@gmail.com>
2017-01-12 14:24:13 -0500

    init commit for grades practice

---
+++ grades/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gradebook book = new Gradebook(); //constructor
+            book.AddGrade(91);
+        }
+    }
+}
